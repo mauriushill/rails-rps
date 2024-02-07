4ab704d9f49392efd687333b1be50332f233596d
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-07 15:43:00 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get("/paper", { :controller => "zebra", :action => "sun" })
 
   get("/scissors", { :controller => "zebra", :action => "mars" })
-  
+
   get("/", { :controller => "zebra", :action => "rules" })
 end
